ecf2f2aaf6b07aa1dd84ec889f865e9eeb9f29ce
Edmundo Bautista Reyes <rtv.enlinea@hotmail.com>
2020-10-15 23:06:23 -0500

    Agregar código al repositorio

---
+++ demoTablas/demoTablas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace demoTablas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("\nTabla de multiplicar del {0}", i);
+                Console.WriteLine("------------------------------");
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", i, j, (i * j));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
